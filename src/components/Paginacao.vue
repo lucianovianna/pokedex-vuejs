b4873c2e8c1f5d724fef1ab54faa14e2e35d660a
lucianovianna <luciano11v11@gmail.com>
2021-02-23 10:57:45 -0300

    Modificando componente raiz Add primeiro componente

---
+++ src/components/Paginacao.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="overflow-auto">
+    <div>
+      <h6>Goto first button number</h6>
+      <b-pagination
+        v-model="currentPage"
+        :total-rows="rows"
+        :per-page="perPage"
+        first-number
+      ></b-pagination>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [rows],
+  data() {
+    return {
+      rows: rows,
+      currentPage: 1,
+			perPage: 5,
+    };
+  },
+};
+</script>
+
+<style>
+/*  */
+</style>
