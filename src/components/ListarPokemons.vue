a5d8848d3f72fdb08cb2ce9bb52426a572a897dd
lucianovianna <luciano11v11@gmail.com>
2022-05-28 14:09:21 -0300

    melhorando ordenação e filtragem

--- src/components/ListarPokemons.vue
+++ src/components/ListarPokemons.vue
@@ -18,7 +18,7 @@
         </b-input-group>
       </b-col>
       <b-col cols="*">
-        <dropdown-ordenacao @changeOrder="ordernarPokemons($event)"/>
+        <dropdown-ordenacao @changeOrder="setOrdenacao($event)"/>
       </b-col>
     </b-row>
 
@@ -62,14 +62,36 @@ export default {
       pokemonsList: [],
       loading: false,
       busca: "",
+      ordenacao: { 
+        campo: "id", 
+        ordem: "asc" 
+      },
       currentPage: 1,
       perPage: 15,
     };
   },
 
   computed: {
+    pokemonsOrdenados() {
+      const campo = this.ordenacao.campo;
+      const ordem = this.ordenacao.ordem; 
+      let orderedPokemonsList = JSON.parse(JSON.stringify(this.pokemonsList));
+
+      const operation = {
+        "desc": (a, b) => a < b,
+        "asc": (a, b) => a > b
+      };
+
+      if (campo == "id") {
+        orderedPokemonsList.sort((a, b) => operation[ordem](Number(a[campo]), Number(b[campo])) ? 1 : -1);
+      } else {
+        orderedPokemonsList.sort((a, b) => operation[ordem](a[campo], b[campo]) ? 1 : -1);
+      }
+
+      return orderedPokemonsList;
+    },
     pokemonsFiltrados() {
-      return this.pokemonsList.filter(
+      return this.pokemonsOrdenados.filter(
         (poke) => poke.name.toLowerCase().indexOf(this.busca.toLowerCase()) >= 0
       );
     },
@@ -135,19 +157,9 @@ export default {
         }
       });
     },
-    ordernarPokemons(ordenacao) {
-      let campo = ordenacao.campo;
-      let pokemonList = JSON.parse(JSON.stringify(this.pokemonsList));
-      let ordenados = [];
-
-      if (ordenacao.ordem == "asc") {
-        ordenados = pokemonList.sort((a, b) => a[campo] > b[campo] ? 1 : -1);
-      } else {
-        ordenados = pokemonList.sort((a, b) => a[campo] > b[campo] ? -1 : 1);
-      }
-
+    setOrdenacao(ordenacao) {
+      this.ordenacao = ordenacao;
       this.currentPage = 1;
-      this.pokemonsList = ordenados;
     }
   },
 };
