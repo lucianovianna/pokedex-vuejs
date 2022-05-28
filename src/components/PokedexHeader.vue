282c6e6f55e6f66209d3e5f948c935a31240f07b
lucianovianna <luciano11v11@gmail.com>
2022-05-28 12:34:05 -0300

    atualizando icon do header

--- src/components/PokedexHeader.vue
+++ src/components/PokedexHeader.vue
@@ -5,7 +5,7 @@
     <b-navbar variant="faded" type="light"  class="container mb-4">
       <b-navbar-brand href="/">
         <b-img
-          src="https://spng.pngfind.com/pngs/s/103-1032129_pokeball-pokeball-sprite-hd-png-download.png"
+          src="pokeball-icon.png"
           class="d-inline-block align-top"
           width="30px"
           alt=""
